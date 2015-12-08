d9a02de5e5b07f04dcc469b169356c54dc017cea
Dennis Y. Parygin <dyp2000@mail.ru>
2015-12-08 10:42:53 +0500

    Убрал автокомпиляцию

--- sublimerl_lib/__init__.py
+++ sublimerl_lib/__init__.py
@@ -1,14 +1,14 @@
 from .sublimerl_completion import SublimErlCompletionsListener
 from .sublimerl_formatter import SublimErlAutoFormatCommand
-from .sublimerl_autocompiler import SublimErlAutocompilerListener
+# from .sublimerl_autocompiler import SublimErlAutocompilerListener
 from .sublimerl_tests_integration import SublimErlDialyzerCommand, SublimErlTestCommand, SublimErlRedoCommand, SublimErlCtResultsCommand
 from .sublimerl_function_search import SublimErlFunctionSearchCommand
 
 __all__ = ["SublimErlCompletionsListener",
            "SublimErlAutoFormatCommand",
-           "SublimErlAutocompilerListener",
+           # "SublimErlAutocompilerListener",
            "SublimErlDialyzerCommand", 
-           "SublimErlTestCommand", 
+           # "SublimErlTestCommand", 
            "SublimErlRedoCommand", 
            "SublimErlCtResultsCommand",
            "SublimErlFunctionSearchCommand"]
